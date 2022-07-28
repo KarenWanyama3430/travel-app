d17b53d87cbfbc1db769520e72b3a2c1f30840e6
+ <+>
2022-07-28 23:26:05 +0300

    commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -80,4 +80,21 @@ const Section = styled.section`
   }
 `;
 
- 
+ /* API link */
+if ('geolocation' in navigator){
+  console.log ('geolocation available');
+  navigator.geolocation.getCurrentPosition(position => {
+      console.log(position);
+  });
+}else {
+  console.log ('geolocation not available');
+}
+
+const sucessfulLookup = (position) =>{
+  const {lattitude ,longitude}= position.coords;
+  const self= this;
+  fetch ('https://api.opencagedata.com/geocode/v1/json?q=${lattitude}+{longitude}&key=0e4d7d46374b4894a7d31af02107372d')
+  .then (response = response.json())
+  .then (console.log);
+};         
+
