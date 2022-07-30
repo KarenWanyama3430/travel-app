b93aa7fdf395795c51761cc3c30f966e51be26c7
+ <+>
2022-07-30 19:55:13 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -42,3 +42,23 @@ export default function App() {
     </div>
   );
 }
+
+
+
+/* API link */
+if ('geolocation' in navigator){
+  console.log ('geolocation available');
+  navigator.geolocation.getCurrentPosition(position => {
+      console.log(position);
+  });
+}else {
+  console.log ('geolocation not available');
+}
+
+const sucessfulLookup = (position) =>{
+  const {lattitude ,longitude}= position.coords;
+  const self= this;
+  fetch ('https://api.opencagedata.com/geocode/v1/json?q=${lattitude}+{longitude}&key=0e4d7d46374b4894a7d31af02107372d')
+  .then (response = response.json())
+  .then (console.log);
+};         
